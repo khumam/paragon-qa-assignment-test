0f7abea8c54b6cf1c564559443915fbdc94ad966
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2025-06-29 20:12:34 +0700

    add another order process

--- cypress/e2e/2-order/order-success.cy.js
+++ cypress/e2e/2-order/order-success.cy.js
@@ -24,6 +24,29 @@ describe("Order Flow", () => {
     cy.get('[class*="HeaderQbee_total-cart_"]').click();
     cy.get('input[type="checkbox"].chakra-checkbox__input').check({force: true});
     cy.contains("button", /beli sekarang/i).click();
+    cy.contains(/Total pembayaran/i).should("be.visible");
+    cy.contains(/pilih layanan pengiriman/i).click();
+    cy.get('input[type="radio"][name="delivery-opt"]').first().check();
+    cy.get("[class*=pickCourier_courier-container_").first().click();
+    cy.contains("button", /pilih pembayaran/i).click();
+    cy.contains(/pesanan berhasil dipesan/i);
+  });
+
+  it("should success order flow from add to cart, then access the cart and process the order", () => {
+    cy.get("a").contains(/masuk/i).click();
+    cy.contains(/nomor hp/i).click();
+    cy.fixture("user").then((user) => {
+      cy.get('input[name="phone"]').type(user.phone);
+      cy.get('input[name="password"]').type(user.password);
+    });
+    cy.contains("button", /masuk/i).click();
+    cy.url().should("not.include", "/login");
+    cy.contains(/khoerul/i);
+    cy.contains("button", /keranjang/i).first().click();
+    cy.get('[class*="HeaderQbee_total-cart_"]').click();
+    cy.get('input[type="checkbox"].chakra-checkbox__input').check({force: true});
+    cy.contains("button", /beli sekarang/i).click();
+    cy.contains(/Total pembayaran/i).should("be.visible");
     cy.contains(/pilih layanan pengiriman/i).click();
     cy.get('input[type="radio"][name="delivery-opt"]').first().check();
     cy.get("[class*=pickCourier_courier-container_").first().click();
